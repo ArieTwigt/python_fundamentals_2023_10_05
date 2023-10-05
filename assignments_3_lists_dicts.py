a781cf4b7be278cc8710d117ba22ee1a22541d32
Arie Twigt <twigt.arie@gmail.com>
2023-10-05 13:56:09 +0200

    added assignment 2

--- assignments_3_lists_dicts.py
+++ assignments_3_lists_dicts.py
@@ -5,12 +5,47 @@ person_1['age'] = 30
 person_1['hobbies'] = ['surfing', 'reading', 'cycling']
 
 print(person_1)
+
+
+# %% Assignment 2. Family
+
+# first create the imagionary persons
+person_1 = {}
+person_1['name'] = "James"
+person_1['age'] = 30
+person_1['hobbies'] = ['surfing', 'reading', 'cycling']
+
+
+person_2 = {}
+person_2['name'] = "Mary"
+person_2['age'] = 35
+person_2['hobbies'] = ['football', 'reading', 'programming']
+
+person_3 = {}
+person_3['name'] = "Jimmy"
+person_3['age'] = 8
+person_3['hobbies'] = ['gaming', 'tennis']
+
+person_4 = {}
+person_4['name'] = "Bob"
+person_4['age'] = 4
+person_4['hobbies'] = ['lego', 'math', 'running']
+
+# create the family dictionary
+family_dict = {}
+family_dict['name'] = "Jones"
+family_dict['members'] = [person_1, person_2, person_3, person_4]
+
+#%%
+family_dict
+
+
+
+
+
+
+
+
+
+
 # %%
-
-
-
-
-
-
-
-
