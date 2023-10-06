5ac78f4dee5f12b9a43c234bd6fd1e2371225ba1
Arie Twigt <twigt.arie@gmail.com>
2023-10-06 11:17:48 +0200

    added if statements to rdw

--- assignment_5_conditional_statements.py
+++ assignment_5_conditional_statements.py
@@ -2,7 +2,7 @@
 
 name = "Arie"
 
-if name[0].lower():
+if name[0].lower() == 'a':
     print(f"Your name starts with an 'A': {name}")
 else:
     print(f"Your name does not with an 'A': {name}")
@@ -11,7 +11,7 @@ else:
 
 #%% Assignment 2a.
 
-vowels = "aeoui"
+vowels = ['a', 'e', 'i', 'u', 'o']
 
 name = "Dirk"
 
@@ -26,28 +26,38 @@ else:
     new_name = name.replace(first_letter, "A")
     print(new_name)
 
+
+
+
+
+
+
 # %% Assignment b. Random
 import string
 import random
 
 # create the vowels and non-vowels
 vowels = "aeoui"
-non_vowels = string.ascii_lowercase
-non_vowels = [letter for letter in non_vowels if letter not in vowels]
+letters = string.ascii_lowercase
+non_vowels = [letter for letter in letters if letter not in vowels]
 
 # get a random letter
-name = "Dirk"
+name = "Arie"
 
 first_letter = name[0]
 
 if first_letter.lower() in vowels:
-    random_letter = random.choice(vowels)
-    new_name = name.replace(first_letter, random_letter.upper())
-    print(f"{name} -> {new_name}")
-else:
     random_letter = random.choice(non_vowels)
     new_name = name.replace(first_letter, random_letter.upper())
     print(f"{name} -> {new_name}")
+else:
+    random_letter = random.choice(vowels)
+    new_name = name.replace(first_letter, random_letter.upper())
+    print(f"{name} -> {new_name}")
+
+
+
+
 
 
 #%% Assignment 3. - Create a conditional statement that indicates if your age is between 18 and 68.
@@ -55,13 +65,13 @@ else:
 age = 34
 
 
-if 18 < age < 68:
+if 18 <= age <= 68:
     print("You are between 18 and 68")
 else:
     print("You are not") 
 
 # %%
-if age > 18 and age < 68:
+if age >= 18 and age <= 68:
     print("You are between 18 and 68")
 else:
     print("You are not") 
--- import_rdw.py
+++ import_rdw.py
@@ -1,4 +1,6 @@
 import requests
+import sys
+import os
 
 # specify the brand
 selected_brand = input("🚗 Insert a brand:\n")
@@ -12,10 +14,31 @@ endpoint = f"https://opendata.rdw.nl/resource/m9d7-ebf2.json?merk={selected_bran
 # execute the request
 response = requests.get(endpoint)
 
+# check if the response code is 200
+if not response.status_code == 200:
+    print("Something went wrong")
+    print(f"{response.status_code}")
+    sys.exit()
 
 # get the data
 data = response.json()
 
+# check if the list is empty
+if len(data) == 0:
+    print(f"No cars found for the brand {selected_brand}")
+    sys.exit()
+
 # show the first 5 cars
 print(data[:5])
+
+# create the folder for this brand if it does not exist alrady
+files_folders = os.listdir()
+
+# check if the folder already exists
+if selected_brand_upper not in files_folders:
+    print(f"📂 Creating folder '{selected_brand_upper}'")
+    os.mkdir(selected_brand_upper)
+
+
+
 pass
